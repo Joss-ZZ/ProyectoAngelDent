e096921034213e4fb1f8b56d7c845a0eb930bffb
Joss-ZZ <coderjzz@gmail.com>
2020-07-17 21:21:16 -0500

    Borrando comentario

--- app/src/main/java/com/example/proyecto/MainActivity.java
+++ app/src/main/java/com/example/proyecto/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ProgressBar;
-//dsfsfsfsfscscs
 
 public class MainActivity extends AppCompatActivity {
     ProgressBar jpbCarga;
