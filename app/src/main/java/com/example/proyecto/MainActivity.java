1ef0b424c1b79c66788e052d706d6423d9374341
bryandeal826 <bryandeal826@gmail.com>
2020-07-17 21:13:14 -0500

    raul feed

--- app/src/main/java/com/example/proyecto/MainActivity.java
+++ app/src/main/java/com/example/proyecto/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ProgressBar;
+//dsfsfsfsfscscs
 
 public class MainActivity extends AppCompatActivity {
     ProgressBar jpbCarga;
